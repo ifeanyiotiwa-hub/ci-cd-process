74f0ef7d69b7f29a2d7e38a99f279716bca91dc0
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-16 16:23:22 +0100

    OTIWA & ELVIS | ci.yml | Added master to line 7

--- src/main/java/dev/gxsoft/GxsoftApplication.java
+++ src/main/java/dev/gxsoft/GxsoftApplication.java
@@ -8,11 +8,10 @@ public class GxsoftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GxsoftApplication.class, args);
-
 		System.out.println(multiplyTwoInts(5, 10));
 	}
 
-	public static int multiplyTwoInts(int x, int y){
+	public static int multiplyTwoInts(int x, int y) {
 		return x * y;
 	}
 
