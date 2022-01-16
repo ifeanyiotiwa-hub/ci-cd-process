cfd471a6c81464735c85f07e86a2a4827f1632af
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-16 20:17:47 +0100

    should pass

--- src/main/java/dev/gxsoft/GxsoftApplication.java
+++ src/main/java/dev/gxsoft/GxsoftApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GxsoftApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(GxsoftApplication.class, args);
     System.out.println(multiplyTwoInts(5, 10));
