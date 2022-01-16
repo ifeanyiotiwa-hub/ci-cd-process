12f1f1899e0ee3b899ba8b88118c9e8d6b66ae6f
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-16 13:53:39 +0100

    Initialize application

---
+++ src/main/java/dev/gxsoft/GxsoftApplication.java
@@ -0,0 +1,13 @@
+package dev.gxsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GxsoftApplication.class, args);
+	}
+
+}
